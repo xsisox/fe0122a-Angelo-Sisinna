b1cd92d20f05d7f0f807953fdd13dd09e898e957
xsisox <a.sisinna@gmail.com>
2022-02-17 23:36:03 +0100

    commenti

--- s4/progetto/asset/javascript/script.js
+++ s4/progetto/asset/javascript/script.js
@@ -1,15 +1,27 @@
-function resetCalcolatrice(){
-    let risultato = document.getElementById("risultato")
+
+//creo la funzione per il reset del tasto C//creo la variable e il metodo da inserire in html tramite id //assegno a alla variabile risultato un valore di 'reset/campovuoto' 
+
+function resetCalcolatrice(){ 
+    let risultato = document.getElementById("risultato") 
     risultato.value = ''
 }
 
-function totale(){
-    let res = document.getElementById("risultato")
-    let totale = eval(res.value)
-    res.value = totale
-}
 
-function prendiTasto(tasto){
+//creo la funzione per il totale del tasto =//creo la variable e il metodo// eval serve a valutare un'espressione 
+
+/*function totale(){ 
+    let res = document.getElementById("risultato") 
+    let totale = eval(res.value); 
+}*/
+
+
+function totale() { 
+    document.getElementById("risultato").value=eval(document.getElementById("risultato").value); 
+    } 
+
+// creo la funzione del tasto  //creo la variabile e il metodo  // inserisco id parametri tasto//da un numero al valore
+
+function prendiTasto(tasto){ 
     let risultato = document.getElementById("risultato")
     let num = tasto.id;
     risultato.value += num 
